98044304a709b81c7756f9d69d017bae128473c0
Exyloss <antonin.pons@protonmail.com>
2022-06-30 16:29:15 +0200

    commit

--- integrale.py
+++ integrale.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
 def integrale_rectangle(f, a, b, n=100000):
+    """
+    f: fonction à intégrer
+    a: début de d'intégrale
+    b: fin de l'intégrale
+    n: nombre de rectangles
+    """
     delta = b-a
     largeur = delta/n
     integrale = 0
@@ -9,6 +15,12 @@ def integrale_rectangle(f, a, b, n=100000):
     return integrale
 
 def integrale_trapeze(f, a, b, n=100000):
+    """
+    f: fonction à intégrer
+    a: début de d'intégrale
+    b: fin de l'intégrale
+    n: nombre de rectangles
+    """
     delta = b-a
     largeur = delta/n
     integrale = 0
@@ -16,9 +28,31 @@ def integrale_trapeze(f, a, b, n=100000):
         integrale += largeur*(f(a+largeur*i)+f(a+largeur*(i-1)))/2
     return integrale
 
+def integrale_montecarlo(f, a, b, h, n=100000):
+    """
+    f: fonction à intégrer
+    a: début de d'intégrale
+    b: fin de l'intégrale
+    h: hauteur maximale de f
+    n: nombre de tirages
+    """
+    from random import random
+    somme = 0
+    delta=b-a
+    aire_totale = delta*h
+    for _ in range(n):
+        x = a+delta*random()
+        y = random()*h
+        if f(x) > y:
+            somme += 1
+    moyenne = somme/n
+    integrale = moyenne * aire_totale
+    return integrale
+
 if __name__ == "__main__":
     def f(x):
         return x**2
-    print(integrale_rectangle(f, 0, 3))
-    print(integrale_trapeze(f, 0, 3))
+    print("Méthode des rectangles :", integrale_rectangle(f, 0, 3))
+    print("Méthode des trapèzes :", integrale_trapeze(f, 0, 3))
+    print("Méthode de Monte-Carlo :", integrale_montecarlo(f, 0, 3, 9))
 
