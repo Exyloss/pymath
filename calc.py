5b7279d2fa50fd2983dbf73e8a6c130692055987
Exyloss <antoninpons@protonmail.com>
2022-07-01 09:47:50 +0200

    commit

--- calc.py
+++ calc.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # Calculatrice en cours de développement...
-from math import exp, pi, sin, cos, sqrt
+from math import exp, pi, sin, cos, sqrt, e
 from arithmetique import solve_diophantienne
+from solve import *
 r = "h"
 while r != "q":
     if r == "h":
@@ -22,6 +23,35 @@ while r != "q":
                 except:
                     print("erreur de syntaxe")
                     pass
+    elif r == "3":
+            try:
+                a = int(input("a:"))
+                b = int(input("b:"))
+                c = int(input("c:"))
+                signe = "+"
+                b_str = str(b)+"x"
+                signe2 = "+"
+                c_str = str(c)
+                if b < 0:
+                    signe1 = "-"
+                    b_str = str(abs(b))+"x"
+                elif b == 0:
+                    signe1 = ""
+                    b_str = ""
+                if c < 0:
+                    signe2 = "-"
+                    c_str = str(abs(c))
+                elif c == 0:
+                    signe2 = ""
+                    c_str = ""
+
+                print("équation : "+str(a)+"x^2"+" "+signe1+" "+b_str+" "+signe2+" "+c_str+" = 0")
+                print(solve_snd_deg(a, b, c))
+            except:
+                print("erreur")
+                pass
+            r = input("quitter = q:")
+
     elif r == "4":
         while r != "q":
             try:
